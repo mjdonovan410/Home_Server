d71f26e54ec10a0e0c8001d1519458ef2895b835
Donovan <matt.donovan@premier-system.com>
2019-10-03 11:05:44 -0500

    Fixed modal not closing

--- react-client/src/Components/TempWidget.js
+++ react-client/src/Components/TempWidget.js
@@ -13,12 +13,11 @@ function TempWidget(props) {
         return ((today.getHours() < 10)?"0":"") + today.getHours() +":"+ ((today.getMinutes() < 10)?"0":"") + today.getMinutes() +":"+ ((today.getSeconds() < 10)?"0":"") + today.getSeconds();
     }
 
-    function loadModal(){
+    const loadModal = () =>{
         setModal(true)
     }
 
-    function deleteModal(){
-        console.log(tempModal)
+    const deleteModal = () =>{
         setModal(false)
     }
     
@@ -44,23 +43,25 @@ function TempWidget(props) {
     },[]);
 
     return (
-        <div className='widgetCont' onClick={loadModal}>
-            {(tempModal) ? <ModalTemplate deleteModal={deleteModal}/> : null}
-            <div className='tempWidgetCircle'>
-                {
-                    (loadFault || deviceData.temp !== "#") ? 
-                    <p className='tempNum'>{deviceData.temp}</p> 
-                    : <img src="./images/loading.gif" width="80px" className="tempLoading"/> 
-                }
-                <div className='tempRainDrop' style={{backgroundImage:"url('./images/raindrop.png')", height:'50px', width:'35px'}}>
-                    <p>{deviceData.humid}</p>
+        <div>
+            {tempModal && <ModalTemplate deleteModal={deleteModal}/>}
+            <div className='widgetCont' onClick={loadModal}>
+                <div className='tempWidgetCircle'>
+                    {
+                        (loadFault || deviceData.temp !== "#") ? 
+                        <p className='tempNum'>{deviceData.temp}</p> 
+                        : <img src="./images/loading.gif" width="80px" className="tempLoading"/> 
+                    }
+                    <div className='tempRainDrop' style={{backgroundImage:"url('./images/raindrop.png')", height:'50px', width:'35px'}}>
+                        <p>{deviceData.humid}</p>
+                    </div>
                 </div>
+                <div className="tempDetails">
+                    <p style={{fontSize:"24px", fontWeight:"bold"}}>{ deviceData.name }</p>
+                    <p style={{fontSize:"18px"}}>{ deviceData.ipAddress }</p>
+                </div>
+                <p className="tempUpdated">Last Updated: { lastUpdate }</p>
             </div>
-            <div className="tempDetails">
-                <p style={{fontSize:"24px", fontWeight:"bold"}}>{ deviceData.name }</p>
-                <p style={{fontSize:"18px"}}>{ deviceData.ipAddress }</p>
-            </div>
-            <p className="tempUpdated">Last Updated: { lastUpdate }</p>
         </div>
     );
 }
