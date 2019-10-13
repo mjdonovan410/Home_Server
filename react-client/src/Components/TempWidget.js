f81e4047a2e66781ac73dcc52b6e1f026c7ad894
Matt Donovan <mjdonovan410@gmail.com>
2019-10-13 12:41:35 -0500

    Added hue change for different temps. Moved temp widget styles

--- react-client/src/Components/TempWidget.js
+++ react-client/src/Components/TempWidget.js
@@ -1,18 +1,69 @@
-import React, {useEffect, useState} from 'react';
-import fetch from "../fetchWithTimeout"
+import React, {useEffect, useState} from 'react'
+import {createUseStyles} from 'react-jss'
 import ModalTemplate from "./ModalTemplate"
 import TempModalContent from "./TempModalContent"
 
 function TempWidget(props) {
-    const [deviceData, setData] = useState({"name":props.sensor.name, "temp":"#", "humid":"", "ipAddress":props.sensor.ipAddress});
-    const [loadFault, setLoadFault] = useState(false);
-    const [lastUpdate, setUpdate] = useState("");
     const [tempModal, setModal] = useState(false);
+    let classes;
+    let temp = (props.sensor.temp != "#") ? parseInt(props.sensor.temp) : 90;
+    let hueColor = -0.0000233100233099533*Math.pow(temp,4)+0.00403522403520387*Math.pow(temp,3)-0.0144133644112692*Math.pow(temp,2)-29.8168868169803*temp+1387.71173271325
+    hueColor = (hueColor>275) ? 275 : hueColor;
+    let styleProps = {hueColor};
 
-    function getTime(){
-        var today = new Date();
-        return ((today.getHours() < 10)?"0":"") + today.getHours() +":"+ ((today.getMinutes() < 10)?"0":"") + today.getMinutes() +":"+ ((today.getSeconds() < 10)?"0":"") + today.getSeconds();
-    }
+    const useStyles = createUseStyles({ 
+        tempWidgetCircle:{
+            height: '90px',
+            width: '90px',
+            borderRadius: '50%',
+            alignSelf: 'center',
+            justifySelf: 'left',
+            marginLeft: '5px',
+            color: '#20232A',
+            display: 'grid',
+            position: 'relative',
+            border: styleProps => '5px solid hsl('+styleProps.hueColor+',100%,50%)',
+        },
+        tempNum:{
+            placeSelf: 'center',
+            fontSize: '48px',
+            margin: '0',
+        },
+        tempRainDrop:{
+            position: 'absolute',
+            bottom: '-8px',
+            right: '-10px',
+            display: 'grid',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+            backgroundImage:"url('./images/raindrop.png')",
+        },
+        tempRainDropP:{
+            placeSelf: 'center',
+            fontSize: '24px',
+            margin: '10px auto 0px auto', 
+            color: 'white',
+        },
+        tempDetails:{
+            color: '#20232A',
+            display: 'grid',
+        },
+        tempDetailsP:{
+            margin: '0px',
+            placeSelf: 'center',
+        },
+        tempLoading:{
+            placeSelf: 'center',
+        },
+        tempUpdated:{
+            position: 'absolute',
+            fontSize: '12px',
+            bottom: '1px',
+            right: '3px',
+            margin: '0px',
+        },
+    })
 
     const loadModal = () =>{
         setModal(true)
@@ -22,21 +73,22 @@ function TempWidget(props) {
         setModal(false)
     }
 
+    classes = useStyles(styleProps);
     return (
         <div>
             {tempModal && <ModalTemplate deleteModal={deleteModal} content={<TempModalContent sensor={props.sensor}/>}/>}
             <div className='widgetCont' onClick={loadModal}>
-                <div className='tempWidgetCircle'>
-                    <p className='tempNum'>{props.sensor.temp}</p>
-                    <div className='tempRainDrop' style={{backgroundImage:"url('./images/raindrop.png')", height:'50px', width:'35px'}}>
-                        <p>{props.sensor.humid}</p>
+                <div className={classes.tempWidgetCircle}>
+                    <p className={classes.tempNum}>{props.sensor.temp}</p>
+                    <div className={classes.tempRainDrop} style={{backgroundImage:"url('./images/raindrop.png')", height:'50px', width:'35px'}}>
+                        <p className={classes.tempRainDropP}>{props.sensor.humid}</p>
                     </div>
                 </div>
-                <div className="tempDetails">
-                    <p style={{fontSize:"24px", fontWeight:"bold"}}>{ props.sensor.name }</p>
-                    <p style={{fontSize:"18px"}}>{ props.sensor.ipAddress }</p>
+                <div className={classes.tempDetails}>
+                    <p className={classes.tempDetailsP} style={{fontSize:"24px", fontWeight:"bold"}}>{ props.sensor.name }</p>
+                    <p className={classes.tempDetailsP} style={{fontSize:"18px"}}>{ props.sensor.ipAddress }</p>
                 </div>
-                <p className="tempUpdated">Last Updated: { props.sensor.updated }</p>
+                <p className={classes.tempUpdated}>Last Updated: { props.sensor.updated }</p>
             </div>
         </div>
     );
